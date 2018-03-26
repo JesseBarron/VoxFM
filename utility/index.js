3b3536c69fd7788feddc2d58bc6d0f8b680a82a9
Jesse Barron <jessebarron1113@gmail.com>
2018-03-25 20:30:50 -0500

    Changed Utility

--- utility/index.js
+++ utility/index.js
@@ -2,4 +2,3 @@ import { Platform, NativeModules } from 'react-native'
 
 
 export const ShoutStreamer = Platform.OS === 'ios' ? NativeModules.ShoutStreamerIOS : null
-export const VideoPlayer = Platform.OS === 'ios' ? NativeModules.VideoPlayerIOS : null
